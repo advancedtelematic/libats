369e076bf878386626c2050360755ebc43a0ad98
Simão Mata <me@simao.io>
2017-05-31 17:24:57 +0200

    refactor circe codecs

---
+++ libats/src/main/scala/com/advancedtelematic/libats/http/AnyvalMarshallingSupport.scala
@@ -0,0 +1,14 @@
+package com.advancedtelematic.libats.http
+
+import akka.http.scaladsl.unmarshalling.FromStringUnmarshaller
+
+object AnyvalMarshallingSupport {
+  import shapeless._
+
+  private def anyvalFromEntityUnmarshaller[Wrapper <: AnyVal, Wrapped](implicit gen: Generic.Aux[Wrapper, Wrapped :: HNil],
+                                                                       um: FromStringUnmarshaller[Wrapped]): FromStringUnmarshaller[Wrapper] =
+    um.map { v ⇒ gen.from(v :: HNil) }
+
+  implicit def anyvalStringUnmarshaller[Wrapper <: AnyVal]
+  (implicit gen: Generic.Aux[Wrapper, String :: HNil]): FromStringUnmarshaller[Wrapper] = anyvalFromEntityUnmarshaller[Wrapper, String]
+}
