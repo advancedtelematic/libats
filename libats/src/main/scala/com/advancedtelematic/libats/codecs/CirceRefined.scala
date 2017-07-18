36e55c69ad019cabdb2de4c5caecc49a4bb4449e
Simão Mata <me@simao.io>
2017-07-18 14:12:26 +0200

    fix refined codec

--- libats/src/main/scala/com/advancedtelematic/libats/codecs/CirceRefined.scala
+++ libats/src/main/scala/com/advancedtelematic/libats/codecs/CirceRefined.scala
@@ -4,17 +4,19 @@ import eu.timepit.refined.api.{Refined, Validate}
 import eu.timepit.refined.refineV
 import io.circe.{Decoder, Encoder}
 
+import scala.util.{Failure, Success}
+
 trait CirceRefined {
   implicit def refinedEncoder[T, P](implicit encoder: Encoder[T]): Encoder[Refined[T, P]] =
     encoder.contramap(_.value)
 
   implicit def refinedDecoder[T, P](implicit decoder: Decoder[T], p: Validate.Plain[T, P]): Decoder[Refined[T, P]] =
-    decoder.map(t =>
+    decoder.emapTry { t =>
       refineV[P](t) match {
-        case Left(e)  =>
-          throw DeserializationException(RefinementError(t, e))
-        case Right(r) => r
-      })
+        case Left(e) => Failure(DeserializationException(RefinementError(t, e)))
+        case Right(r) => Success(r)
+      }
+    }
 }
 
 object CirceRefined extends CirceRefined
