ec804316464ac85716b99a9f4fed6385c3ce55e1
Simão Mata <simao.mata@here.com>
2018-08-09 13:51:40 +0200

    Added test for handleIntegrityErrors

--- deploy/ci_setup.sh
+++ deploy/ci_setup.sh
@@ -22,7 +22,7 @@ docker run -d \
   -e MYSQL_ROOT_PASSWORD=root \
   -e MYSQL_USER=libats_slick \
   -e MYSQL_PASSWORD=libats_slick \
-  mariadb:10.1 \
+  mariadb:10.2 \
   --character-set-server=utf8 --collation-server=utf8_unicode_ci \
   --max_connections=1000
 
--- libats-slick/src/test/resources/db/migration/V1__initial_schema.sql
+++ libats-slick/src/test/resources/db/migration/V1__initial_schema.sql
@@ -5,6 +5,15 @@ create table `books` (
 )
 ;
 
+create table `book_meta` (
+  `book_id` INTEGER NOT NULL,
+  `id` INTEGER NOT NULL,
+  `tag` INTEGER NOT NULL,
+  PRIMARY KEY (`book_id`, `id`),
+  CONSTRAINT `book_id_fk` FOREIGN KEY (`book_id`) REFERENCES `books`(`id`)
+)
+;
+
 create table `re_key_spec`(
   `id` INTEGER NOT NULL PRIMARY KEY,
   `uuid` CHAR(36) NOT NULL UNIQUE,
