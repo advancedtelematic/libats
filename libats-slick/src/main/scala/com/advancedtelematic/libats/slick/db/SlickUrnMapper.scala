a11e6c73306b8ef8546ea42d33bba66e309e59aa
Ignacio Lucero <ignacio.lucero@here.com>
2018-11-16 10:19:55 +0100

    Fix Slick mapping for CorrelationId.
    Signed-off-by: Ignacio Lucero <ignacio.lucero@here.com>

--- libats-slick/src/main/scala/com/advancedtelematic/libats/slick/db/SlickUrnMapper.scala
+++ libats-slick/src/main/scala/com/advancedtelematic/libats/slick/db/SlickUrnMapper.scala
@@ -1,17 +1,13 @@
 package com.advancedtelematic.libats.slick.db
 
+import cats.syntax.either._
 import com.advancedtelematic.libats.data.DataType.CorrelationId
 import slick.jdbc.MySQLProfile.api._
-import io.circe.parser.decode
-import io.circe.syntax._
 
 object SlickUrnMapper {
   implicit val correlationIdMapper = MappedColumnType.base[CorrelationId, String](
-    _.asJson.noSpaces,
-    x => decode[CorrelationId](x) match {
-      case Right(x) => x
-      case Left(err) => throw new Exception(err)
-    }
+    _.toString,
+    CorrelationId.fromString(_).valueOr(s => throw new IllegalArgumentException(s))
   )
 }
 
--- libats/src/main/scala/com/advancedtelematic/libats/data/DataType.scala
+++ libats/src/main/scala/com/advancedtelematic/libats/data/DataType.scala
@@ -1,9 +1,11 @@
 package com.advancedtelematic.libats.data
 
+import java.util.UUID
+
 import com.advancedtelematic.libats.data.DataType.HashMethod.HashMethod
 import eu.timepit.refined.api.{Refined, Validate}
-import java.util.UUID
 import io.circe.{Decoder, Encoder}
+
 import scala.language.postfixOps
 
 object DataType {
@@ -23,25 +25,26 @@ object DataType {
     ValidationUtils.validHexValidation(ValidChecksum(), length = 64)
 
   sealed trait CorrelationId
-  final case class CampaignId(value: UUID) extends CorrelationId
-  final case class MultiTargetUpdateId(value: UUID) extends CorrelationId
+  final case class CampaignId(value: UUID) extends CorrelationId {
+    override def toString: String = s"urn:here-ota:campaign:$value"
+  }
+  final case class MultiTargetUpdateId(value: UUID) extends CorrelationId {
+    override def toString: String = s"urn:here-ota:mtu:$value"
+  }
 
   object CorrelationId {
     private[this] val CorrelationIdRe = """^urn:here-ota:(mtu|campaign):([0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12})$"""r
 
-    implicit val DecoderInstance: Decoder[CorrelationId] = Decoder.decodeString.emap {
+    def fromString(s: String): Either[String, CorrelationId] = s match {
       case CorrelationIdRe("mtu", uuid) =>
         Right(MultiTargetUpdateId(UUID.fromString(uuid)))
-
       case CorrelationIdRe("campaign", uuid) =>
         Right(CampaignId(UUID.fromString(uuid)))
       case x =>
-        Left(s"'$x' is an invalid CorrelationId")
+        Left(s"Invalid correlationId: '$x'.")
     }
 
-    implicit val EncoderInstance: Encoder[CorrelationId] = Encoder.encodeString.contramap {
-      case CampaignId(x) => s"urn:here-ota:campaign:$x"
-      case MultiTargetUpdateId(x) => s"urn:here-ota:mtu:$x"
-    }
+    implicit val DecoderInstance: Decoder[CorrelationId] = Decoder.decodeString.emap(CorrelationId.fromString)
+    implicit val EncoderInstance: Encoder[CorrelationId] = Encoder.encodeString.contramap(_.toString)
   }
 }
