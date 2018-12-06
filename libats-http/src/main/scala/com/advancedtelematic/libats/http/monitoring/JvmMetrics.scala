3d406228f81b2d704ae81b72fd0445c40fe9e80c
Ignacio Lucero <ignacio.lucero@here.com>
2018-12-06 15:05:53 +0100

    Fix some deprecation warnings.
    Signed-off-by: Ignacio Lucero <ignacio.lucero@here.com>

--- libats-http/src/main/scala/com/advancedtelematic/libats/http/monitoring/JvmMetrics.scala
+++ libats-http/src/main/scala/com/advancedtelematic/libats/http/monitoring/JvmMetrics.scala
@@ -3,10 +3,10 @@ package com.advancedtelematic.libats.http.monitoring
 import akka.http.scaladsl.util.FastFuture
 import com.advancedtelematic.libats.http.HealthMetrics
 import com.codahale.metrics.jvm.{GarbageCollectorMetricSet, MemoryUsageGaugeSet}
-import com.codahale.metrics.{Metric, MetricFilter, MetricRegistry, MetricSet}
+import com.codahale.metrics.{MetricFilter, MetricRegistry, MetricSet}
 import io.circe.Json
 import io.circe.syntax._
-import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
 import scala.concurrent.Future
 
 
@@ -14,7 +14,7 @@ trait JvmMetricsSupport {
   self: MetricsSupport =>
 
   private def registerAll(registry: MetricRegistry, prefix: String, metricSet: MetricSet): Unit = {
-    metricSet.getMetrics.foreach {
+    metricSet.getMetrics.asScala.foreach {
       case (metricPrefix, set: MetricSet) =>
         registerAll(registry, prefix + "." + metricPrefix, set)
       case (metricPrefix, metric) =>
@@ -27,14 +27,12 @@ trait JvmMetricsSupport {
 }
 
 class JvmMetrics(metrics: MetricRegistry) extends HealthMetrics {
-  val filter = new MetricFilter {
-    override def matches(name: String, metric: Metric): Boolean = name.startsWith("jvm")
-  }
+  val filter: MetricFilter = (name: String, _) => name.startsWith("jvm")
 
   override def urlPrefix: String = "jvm"
 
   override def metricsJson: Future[Json] = FastFuture.successful {
-    val jvm = metrics.getGauges(filter)
+    val jvm = metrics.getGauges(filter).asScala
     val data = jvm.mapValues(_.getValue.toString)
     data.toMap.asJson
   }
