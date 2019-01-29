80110254ee345182ade3716a98b10d2cb867bce0
Ignacio Lucero <ignacio.lucero@here.com>
2019-01-29 13:03:19 +0100

    Add convenience constructor for ValidatedGeneric.
    Signed-off-by: Ignacio Lucero <ignacio.lucero@here.com>

--- libats/src/main/scala/com/advancedtelematic/libats/data/EcuIdentifier.scala
+++ libats/src/main/scala/com/advancedtelematic/libats/data/EcuIdentifier.scala
@@ -9,12 +9,13 @@ object EcuIdentifier {
 
   implicit val validatedEcuIdentifier = new ValidatedGeneric[EcuIdentifier, String] {
     override def to(ecuId: EcuIdentifier): String = ecuId.value
-    override def from(s: String): Either[ValidationError, EcuIdentifier] = apply(s)
+    override def from(s: String): Either[ValidationError, EcuIdentifier] =
+      ValidatedGeneric(
+        new EcuIdentifier(s),
+        s.length >= 0 && s.length <= 64,
+        "An ECU identifier must have at least 1 and at most 64 characters."
+      )
   }
-
-  def apply(s: String): Either[ValidationError, EcuIdentifier] =
-    if (s.length < 0 || s.length > 64) Left(ValidationError("An ECU identifier must have at least 1 and at most 64 characters."))
-    else Right(new EcuIdentifier(s))
 
   implicit val ecuIdentifierEncoder: Encoder[EcuIdentifier] = CirceValidatedGeneric.validatedGenericEncoder
   implicit val ecuIdentifierDecoder: Decoder[EcuIdentifier] = CirceValidatedGeneric.validatedGenericDecoder
--- libats/src/main/scala/com/advancedtelematic/libats/data/ValidatedGeneric.scala
+++ libats/src/main/scala/com/advancedtelematic/libats/data/ValidatedGeneric.scala
@@ -4,3 +4,8 @@ trait ValidatedGeneric[T, Repr] {
   def to(t: T): Repr
   def from(r: Repr): Either[ValidationError, T]
 }
+
+object ValidatedGeneric {
+  def apply[T](t: => T, b: Boolean, msg: String): Either[ValidationError, T] =
+    if (b) Right(t) else Left(ValidationError(msg))
+}
