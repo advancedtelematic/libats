690ba78eca8cbd16ffdedaed2bbfd21f486e4cd0
Simão Mata <simao.mata@here.com>
2019-12-03 16:13:46 +0100

    Add metrics support for akka connection pool
    Monitors the akka http server connection count, exposes:
    
    - Current connection count
    - Total number of connections since boot
    - http server configured max connections
    - http client pool configured max connections
    - http client pool configured max open requests
    
    Signed-off-by: Simão Mata <simao.mata@here.com>

---
+++ libats-metrics-akka/src/main/scala/com/advancedtelematic/metrics/AkkaHttpConnectionMetrics.scala
@@ -0,0 +1,72 @@
+package com.advancedtelematic.metrics
+
+import akka.NotUsed
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+import akka.http.scaladsl.server.{ExceptionHandler, RejectionHandler, Route, RoutingLog}
+import akka.http.scaladsl.settings.{ConnectionPoolSettings, ParserSettings, RoutingSettings, ServerSettings}
+import akka.stream.Materializer
+import akka.stream.scaladsl.Flow
+import com.advancedtelematic.libats.http.BootApp
+import com.advancedtelematic.libats.http.monitoring.MetricsSupport
+import com.codahale.metrics.{Gauge, MetricRegistry}
+
+import scala.concurrent.ExecutionContextExecutor
+
+trait AkkaHttpConnectionMetrics {
+  self: BootApp with MetricsSupport =>
+
+  private lazy val serverSettings = ServerSettings(system)
+
+  metricRegistry.register("akka.http.max-connections", new Gauge[Long]() {
+    override def getValue: Long = serverSettings.maxConnections
+  })
+
+  private lazy val connectionPoolSettings = ConnectionPoolSettings(system)
+
+  metricRegistry.register("akka.http.host-connection-pool.max-connections", new Gauge[Long]() {
+    override def getValue: Long = connectionPoolSettings.maxConnections
+  })
+
+  metricRegistry.register("akka.http.host-connection-pool.max-open-requests", new Gauge[Long]() {
+    override def getValue: Long = connectionPoolSettings.maxOpenRequests
+  })
+
+  def withConnectionMetrics(routes: Route, metricRegistry: MetricRegistry)(implicit
+                                                                           routingSettings: RoutingSettings,
+                                                                           parserSettings: ParserSettings,
+                                                                           materializer: Materializer,
+                                                                           routingLog: RoutingLog,
+                                                                           _executionContext: ExecutionContextExecutor = null,
+                                                                           rejectionHandler: RejectionHandler = RejectionHandler.default,
+                                                                           exceptionHandler: ExceptionHandler = null): Flow[HttpRequest, HttpResponse, NotUsed] =
+    AkkaHttpConnectionMetricsRoutes(routes, metricRegistry)(
+      routingSettings = implicitly,
+      parserSettings = implicitly,
+      materializer = implicitly,
+      routingLog = implicitly,
+      executionContext = _executionContext,
+      rejectionHandler = implicitly,
+      exceptionHandler = implicitly)
+}
+
+object AkkaHttpConnectionMetricsRoutes {
+  def apply(routes: Route, metricRegistry: MetricRegistry)(implicit
+                                                           routingSettings: RoutingSettings,
+                                                           parserSettings: ParserSettings,
+                                                           materializer: Materializer,
+                                                           routingLog: RoutingLog,
+                                                           executionContext: ExecutionContextExecutor = null,
+                                                           rejectionHandler: RejectionHandler = RejectionHandler.default,
+                                                           exceptionHandler: ExceptionHandler = null): Flow[HttpRequest, HttpResponse, NotUsed] = {
+    val handler = Route.handlerFlow(routes)
+
+    Flow[HttpRequest]
+      .watchTermination() {
+        case (mat, completionF) =>
+          metricRegistry.counter("akka.http.connections").inc()
+          metricRegistry.counter("akka.http.connected").inc()
+          completionF.onComplete(_ => metricRegistry.counter("akka.http.connected").dec())
+          mat
+      }.via(handler)
+  }
+}
