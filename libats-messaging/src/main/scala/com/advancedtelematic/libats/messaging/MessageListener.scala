1e7107ba0e9663fbbdc828ccc9bdb92bf560dd58
Simão Mata <simao.mata@here.com>
2020-02-12 11:01:14 +0100

    Catch exceptions when MsgOperation fails to create a future
    We currently catch a failed future when MsgOperation returns a failed
    future, but if MsgOperation cannot create a valid future, successful
    or failed, we don't catch that and propagate the exception up.
    
    This change catches exceptions when MsgOperation fails before it can
    produce a valid future.
    
    Signed-off-by: Simão Mata <simao.mata@here.com>

--- libats-messaging/src/main/scala/com/advancedtelematic/libats/messaging/MessageListener.scala
+++ libats-messaging/src/main/scala/com/advancedtelematic/libats/messaging/MessageListener.scala
@@ -1,7 +1,5 @@
 package com.advancedtelematic.libats.messaging
 
-import java.rmi.registry.Registry
-
 import akka.Done
 import akka.actor.{ActorSystem, Props}
 import akka.event.LoggingAdapter
@@ -9,27 +7,39 @@ import akka.http.scaladsl.util.FastFuture
 import com.advancedtelematic.libats.messaging.MsgOperation.MsgOperation
 import com.advancedtelematic.libats.messaging.daemon.MessageBusListenerActor
 import com.advancedtelematic.libats.messaging_datatype.MessageLike
-import com.codahale.metrics.MetricRegistry
 import com.typesafe.config.Config
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.control.NonFatal
 
 
 object MsgOperation {
   type MsgOperation[T] = T => Future[_]
 
   def logFailed[T](fn: MsgOperation[T])(implicit log: LoggingAdapter, ec: ExecutionContext): MsgOperation[T] = (msg: T) => {
-    fn(msg).recoverWith {
-      case ex =>
-        log.error(ex, s"[Fatal] Could not process message $msg")
+    try {
+      fn(msg).recoverWith {
+        case ex =>
+          log.error(ex, s"[Fatal] MsgOperation returned a failed future:  $msg")
+          FastFuture.failed(ex)
+      }
+    } catch {
+      case NonFatal(ex) =>
+        log.error(ex, s"[NonFatal] MsgOperation could not return a valid future: $msg")
         FastFuture.failed(ex)
     }
   }
 
   def recoverFailed[T](fn: MsgOperation[T])(implicit log: LoggingAdapter, ec: ExecutionContext): MsgOperation[T] = (msg: T) => {
-    fn(msg).recoverWith {
-      case ex =>
-        log.error(ex, s"[NonFatal] Could not process message $msg")
+    try {
+      fn(msg).recoverWith {
+        case ex =>
+          log.error(ex, s"[NonFatal] MsgOperation returned a failed future:  $msg")
+          Future.successful(Done)
+      }
+    } catch {
+      case NonFatal(ex) =>
+        log.error(ex, s"[NonFatal] MsgOperation could not return a valid future: $msg")
         Future.successful(Done)
     }
   }
