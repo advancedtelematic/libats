452f88476873825f972b61ed2b86e1e26a532344
Ignacio Lucero <ignacio.lucero@here.com>
2020-04-09 15:38:56 +0200

    Improve method naming.
    Signed-off-by: Ignacio Lucero <ignacio.lucero@here.com>
    
    Expose error as result of the http call in `ServiceHttpClient`
    
    Before this change, we were allowing the caller of `ServiceHttpClient`
    to specify a error handling partial function. This was meant to give
    the user more control over the result of the http request when the
    request was not successful (not 2xx). However, this turned out to be a
    bit of a leaky abstraction, as `ServiceHttpClient` would call that
    error handler PF to get a `Future` and return that, but if that PF
    returned a successful `Future` and the caller then used `map` on
    that `Future`, then the caller would be operating on the result of the
    HTTP call twice. This works, but it's not intuitive and could lead to
    some bugs and some complex code like:
    
    ```
        execHttpWithNamespace2[Unit](namespace, req) {
          case err if err.status == StatusCodes.NotFound => FastFuture.successful(false)
        }.map {
          case resp if resp.httpResponse.status == StatusCodes.NotFound => false
          case _ => true
        }
    ```
    
    Here `FastFuture.successful(false)` is returned from the error handler, but
    that value still returned into the `map` function, as the future is
    still successful.
    
    This is not the only instance of this type of problems. This MR
    changes the Api of `ServiceHttpClient` to completely expose a
    `RemoteServiceError` rather than an entry point for a PF that handles
    it, so the caller of `execHttp*` needs to explicitly handle
    `Either[RemoteServiceError, R]` to get a `R` and decide what to do
    with a possible `Left[RemoteServiceError]`.
    
    Two convenience methods are provided with `import ServiceHttpClient._`
    to handle the most common cases of:
    
    1. Getting `R` out of `Future[Either[RemoteServiceError, R]]`. Calling
    `.ok` on `Future[Either[RemoteServiceError, R]]` will return a
    successful future of `R` if the value is a `Right`, and a failed
    future containing `RemoteServiceError` if the value is `Left`.
    
    2. Handling different types of errors in
    `Future[Either[RemoteServiceError, R]]`, using `handleErrors`. This is
    a common case and is very similar to what was provided previously
    through a partial function defined by the caller of the api. In theory
    this method is not needed as the caller could simply use methods in
    `Future` to unpack the `Either`, but then the caller would always need
    to handle `Right` and all the `Left`, which the caller might not care
    about, because the methods provided by `Future` all require functions
    and not partial functions.
    
    The names for methods in `ServiceHttpClient` were also changed to be
    more explicit. `execHttp` is now named `execHttpUnmarshalled` as we
    always try to unmarshall the response using the decoder provided
    implicitly.
    
    `execHttpFull` returns both the unmarshalled response and the full
    `HttpResponse` obtained by the akka http client.
    
    Signed-off-by: Simão Mata <simao.mata@here.com>

--- libats-http/src/main/scala/com/advancedtelematic/libats/http/ServiceHttpClient.scala
+++ libats-http/src/main/scala/com/advancedtelematic/libats/http/ServiceHttpClient.scala
@@ -5,20 +5,23 @@ import java.util.UUID
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.headers.RawHeader
 import akka.http.scaladsl.unmarshalling.{FromEntityUnmarshaller, Unmarshaller}
 import akka.http.scaladsl.util.FastFuture
 import akka.stream.Materializer
+import cats.syntax.either._
+import cats.syntax.option._
+import com.advancedtelematic.libats.data.DataType.Namespace
 import com.advancedtelematic.libats.data.ErrorRepresentation
+import com.advancedtelematic.libats.http.Errors.RemoteServiceError
+import com.advancedtelematic.libats.http.ServiceHttpClient.{ServiceHttpFullResponse, ServiceHttpFullResponseEither}
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
 import io.circe.{Encoder, Json}
 import org.slf4j.LoggerFactory
-import cats.syntax.option._
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 import scala.reflect.ClassTag
 import scala.util.Try
-
-case class UnmarshalledHttpResponse[T](httpResponse: HttpResponse, unmarshalledResponse: T)
 
 trait ServiceHttpClientSupport {
   def defaultHttpClient(implicit system: ActorSystem, mat: Materializer): HttpRequest => Future[HttpResponse] = {
@@ -27,26 +30,45 @@ trait ServiceHttpClientSupport {
   }
 }
 
+object ServiceHttpClient {
+  type ServiceHttpFullResponseEither[T] = Either[RemoteServiceError, ServiceHttpFullResponse[T]]
+
+  case class ServiceHttpFullResponse[T](httpResponse: HttpResponse, unmarshalled: T)
+
+  implicit class ServiceHttpFullResponseFutureOps[R](value: Future[Either[RemoteServiceError, R]]) {
+    def ok(implicit ec: ExecutionContext): Future[R] =
+      value.flatMap {
+        case Left(err) => FastFuture.failed(err)
+        case Right(v) => FastFuture.successful(v)
+      }
+
+    def handleErrors(pf: PartialFunction[RemoteServiceError, Future[R]])(implicit ec: ExecutionContext): Future[R] = {
+      value.flatMap {
+        case Left(err) if pf.isDefinedAt(err) => pf.apply(err)
+        case Left(err) => FastFuture.failed(err)
+        case Right(v) => FastFuture.successful(v)
+      }
+    }
+  }
+}
+
 abstract class ServiceHttpClient(_httpClient: HttpRequest => Future[HttpResponse])
                                 (implicit system: ActorSystem, mat: Materializer) {
+  import Errors.RemoteServiceError
   import io.circe.syntax._
   import system.dispatcher
-  import Errors.RemoteServiceError
 
   private val log = LoggerFactory.getLogger(this.getClass)
 
   protected implicit val unitFromEntityUnmarshaller: FromEntityUnmarshaller[Unit] = Unmarshaller.strict(_.discardBytes())
 
-  private def defaultErrorHandler[T](): PartialFunction[RemoteServiceError, Future[T]] = PartialFunction.empty
-
   protected def httpClient = _httpClient
 
   protected def execJsonHttp[Res : ClassTag : FromEntityUnmarshaller, Req : Encoder]
-  (request: HttpRequest, entity: Req)
-  (errorHandler: PartialFunction[RemoteServiceError, Future[Res]] = defaultErrorHandler()): Future[Res] = {
+  (request: HttpRequest, entity: Req): Future[Either[RemoteServiceError, Res]] = {
     val httpEntity = HttpEntity(ContentTypes.`application/json`, entity.asJson.noSpaces)
     val req = request.withEntity(httpEntity)
-    execHttp(req)(errorHandler)
+    execHttpUnmarshalled(req)
   }
 
   private def tryErrorParsing(response: HttpResponse)(implicit um: FromEntityUnmarshaller[ErrorRepresentation]): Future[RemoteServiceError] = {
@@ -60,32 +82,32 @@ abstract class ServiceHttpClient(_httpClient: HttpRequest => Future[HttpResponse
     }
   }
 
-  protected def execHttp[T : ClassTag](request: HttpRequest)
-                                      (errorHandler: PartialFunction[RemoteServiceError, Future[T]] = defaultErrorHandler())
-                                      (implicit um: FromEntityUnmarshaller[T]): Future[T] =
-    execHttp2(request)(errorHandler).map(_.unmarshalledResponse)
+  protected def execHttpUnmarshalled[T : ClassTag](request: HttpRequest)
+                                                  (implicit um: FromEntityUnmarshaller[T]): Future[Either[RemoteServiceError, T]] =
+    execHttpFull(request).map { _.map(_.unmarshalled) }
 
 
-  protected def execHttp2[T : ClassTag](request: HttpRequest)
-                                      (errorHandler: PartialFunction[RemoteServiceError, Future[T]] = defaultErrorHandler())
-                                      (implicit um: FromEntityUnmarshaller[T]): Future[UnmarshalledHttpResponse[T]] =
+  protected def execHttpFull[T : ClassTag](request: HttpRequest)
+                                          (implicit um: FromEntityUnmarshaller[T]): Future[Either[RemoteServiceError, ServiceHttpFullResponse[T]]] =
     httpClient(request).flatMap {
       case r @ HttpResponse(status, _, _, _) if status.isSuccess() =>
-        um(r.entity).map(UnmarshalledHttpResponse(r, _))
+        um(r.entity).map(e => ServiceHttpFullResponse(r, e).asRight)
       case r =>
         tryErrorParsing(r).flatMap { error =>
-          if (errorHandler.isDefinedAt(error))
-            errorHandler(error)
-              .map(UnmarshalledHttpResponse(r, _))
-              .recoverWith { case ex => r.discardEntityBytes(); Future.failed(ex) }
-          else {
-            log.debug(s"request failed: $request")
-            val e = error.copy(msg = s"${this.getClass.getSimpleName}|Unexpected response from remote server at ${request.uri}|${request.method.value}|${r.status.intValue()}|${error.msg}")
+          log.debug(s"request failed: $request")
+          val e = error.copy(msg = s"${this.getClass.getSimpleName}|Unexpected response from remote server at ${request.uri}|${request.method.value}|${r.status.intValue()}|${error.msg}")
 
-            Try(r.discardEntityBytes())
+          Try(r.discardEntityBytes())
 
-            FastFuture.failed(e)
-          }
+          FastFuture.successful(e.asLeft)
         }
     }
+
+  protected def execHttpUnmarshalledWithNamespace[T : ClassTag : FromEntityUnmarshaller](namespace: Namespace, request: HttpRequest): Future[Either[RemoteServiceError, T]] =
+    execHttpFullWithNamespace[T](namespace, request).map(_.map(_.unmarshalled))
+
+  protected def execHttpFullWithNamespace[T : ClassTag : FromEntityUnmarshaller](namespace: Namespace, request: HttpRequest): Future[ServiceHttpFullResponseEither[T]] = {
+    val req = request.addHeader(RawHeader("x-ats-namespace", namespace.get))
+    execHttpFull[T](req)
+  }
 }
