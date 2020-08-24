cc6037c61bbbba41051471204e83a160498c9ec4
Ignacio Lucero <ignacio.lucero@here.com>
2020-08-24 15:54:22 +0200

    Rework the MockMessageBus to keep multiple events per key.
    While testing the ECU replacement events, I noticed that the `MockMessageBus`
    only keeps the latest event, because when a new event with the same key
    arrives, it replaces the previous event. So for example, if a device replaces
    two ECUs, I was only able to find the latest ECU replacement event in `received`.
    So I had to rework it a bit to fit my tests.
    
    While doing that, I noticed that we're actually always searching by values and
    ignoring the keys, so this could be simplified and modeled with a humble `Seq`
    or alike.
    
    Also notice this doesn't break the previous `findReceived` API, so all current
    usages should still work when bumping libats. This is tested in an upcoming MR
    on director that makes use of this new version of the bus.
    
    Relates-to: OTA-5130
    Signed-off-by: Ignacio Lucero <ignacio.lucero@here.com>

--- libats-messaging/src/main/scala/com/advancedtelematic/libats/messaging/test/MockMessageBus.scala
+++ libats-messaging/src/main/scala/com/advancedtelematic/libats/messaging/test/MockMessageBus.scala
@@ -1,8 +1,5 @@
 package com.advancedtelematic.libats.messaging.test
 
-import java.util.concurrent.ConcurrentHashMap
-
-import akka.http.scaladsl.util.FastFuture
 import com.advancedtelematic.libats.messaging.MessageBusPublisher
 import com.advancedtelematic.libats.messaging_datatype.MessageLike
 
@@ -13,29 +10,29 @@ class MockMessageBus extends MessageBusPublisher {
 
   private case class ReceivedMsg(tag: ClassTag[_], payload: Any)
 
-  private val received = new ConcurrentHashMap[String, ReceivedMsg]()
+  private var received = Seq.empty[ReceivedMsg]
 
-  def findReceived[T](filterFn: T => Boolean)(implicit msgLike: MessageLike[T]): Option[T] = {
-    val found: T = received.searchValues(1024, (msg: ReceivedMsg) => {
-      val maybeT = if(msg.tag == msgLike.tag) Some(msg.payload.asInstanceOf[T]) else None
+  def findReceivedAll[T](filterFn: T => Boolean)(implicit msgLike: MessageLike[T]): Seq[T] =
+    received
+        .filter(_.tag == msgLike.tag)
+        .map(_.payload.asInstanceOf[T])
+        .filter(filterFn)
 
-      maybeT match {
-        case Some(t) if filterFn(t) => t
-        case _ => null.asInstanceOf[T]
-      }
-    })
+  def findReceivedAll[T](id: String)(implicit msgLike: MessageLike[T]): Seq[T] =
+    findReceivedAll[T]((t: T) => msgLike.id(t) == id)
 
-    Option(found)
+  def findReceived[T](filterFn: T => Boolean)(implicit msgLike: MessageLike[T]): Option[T] =
+    findReceivedAll(filterFn).headOption
+
+  def findReceived[T](id: String)(implicit msgLike: MessageLike[T]): Option[T] = {
+    findReceived[T]((t: T) => msgLike.id(t) == id)
   }
 
-  def findReceived[T](id: String)(implicit msgLike: MessageLike[T]): Option[T] =
-    findReceived[T]((t: T) => msgLike.id(t) == id)
-
   def reset(): Unit =
-    received.clear()
+    received = Seq.empty[ReceivedMsg]
 
   override def publish[T](msg: T)(implicit ex: ExecutionContext, messageLike: MessageLike[T]): Future[Unit] = {
-    received.put(messageLike.id(msg), ReceivedMsg(messageLike.tag, msg))
-    FastFuture.successful(())
+    received = ReceivedMsg(messageLike.tag, msg) +: received
+    Future.unit
   }
 }
